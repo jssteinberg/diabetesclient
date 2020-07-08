626884a8ab4352190f3f7947718a74fe1a5f2074
Jonas <fluehode@gmail.com>
2020-07-08 20:21:59 +0200

    Simplified and fix plotting after fetch.

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -5,9 +5,17 @@
 
 	let bgLoaded = false
 	let canvasEl
+	let bg
+	
+	// Meal variables
+	let meal_1 = '40'
+	let meal_2 = '60'
+	let meal_3 = '80'
+	let meal_4 = '30'
+
 
 	// This function is sent as callback -- runs after data is loaded from API
-	function renderChart (bg) {
+	function renderChart () {
 		let ctx = canvasEl.getContext("2d");
 		let chart = new Chart(ctx, {
 			type: "line",
@@ -26,17 +34,22 @@
 			options: {}
 		});
 
-		bgLoaded = true
 	}
 
 	function load() {
-		bgLoaded = false
-
 		// Calls method from imported file to load data from API. Sends callback `renderChart` function
-		getBg(renderChart)
+		// getBg(renderChart)
+		fetch("http://127.0.0.1:4500/diabetes?m1=30&m2=30&m3=30&m4=30")
+			.then(res => res.json())
+			.then(function (res) {
+
+			// Run callback if provided
+				bg = res
+			});
+		renderChart()
 	}
 
-	// onMount tells the component has been mounted to the DOM -- it's safe to manipulated DOM
+	// onMount tells the component has been mounted to the DOM -- it's safe to manipulate DOM
 	onMount(async () => {
 		load()
 	})
@@ -44,10 +57,21 @@
 
 <h1>Standard BG values</h1>
 
-{#if bgLoaded}
+<input value={meal_1}>
+<input value={meal_2}>
+<input value={meal_3}>
+<input value={meal_4}>
+
+<p>{meal_1}</p>
+<p>{meal_2}</p>
+<p>{meal_3}</p>
+<p>{meal_4}</p>
+
+<!-- {#if bgLoaded} -->
 	<button on:click={load}>Reload standard BG values from API</button> 
-{:else}
-	<p>Loading data from API ...</p>
-{/if}
+<!-- {:else} -->
+	<!-- <p>Loading data from API ...</p> -->
+<!-- {/if} -->
+
 
 <canvas bind:this={canvasEl}></canvas> 
